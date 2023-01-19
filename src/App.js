9a64fdbc35acc662c70abfa1522aa63ed721d5fa
Allan Tavares <allan.tavares@globalhitss.com.br>
2023-01-19 16:41:35 -0300

    finalizando o dia 19/01/2023

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import {Route,Routes,Link,BrowserRouter} from "react-router-dom";
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+import Container from "./components/layouts/Container";
+import NavBar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Projects from "./components/pages/Projects";
 
 
 function App() {
   return (
-    <div className="App">
-       <p>Allan</p>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Container customClass='minHeight' > 
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/projects' element={<Projects />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} />
+        </Routes>
+     </Container>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,16 @@
+import {FAFacebook} from 'react-icons/fa'
+import styles from './Footer.module.css'
+function Footer() {
+    return(
+       <footer className={styles.footer}>
+        <ul className={styles.socialList}>
+         <li> icoone</li>
+         <li> icoone</li>
+         <li> icoone</li>
+        </ul>
+        <p><span>Finanças</span> &copy; 2023</p>
+       </footer>
+    )
+}
+
+export default Footer
